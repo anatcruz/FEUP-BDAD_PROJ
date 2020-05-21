ea6fc02d63103ef1a81c7a65af221d3de5099dda
andrenasx <andre2000.an@gmail.com>
2020-05-21 14:14:22 +0100

    caps

--- Entrega3/triggers/gatilho2_adiciona.sql
+++ Entrega3/triggers/gatilho2_adiciona.sql
@@ -1,13 +1,11 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TRIGGER if not EXISTS InsertEfetuadoValido
-After insert on Efetuado
-WHEN New.idCompra is NULL
+CREATE TRIGGER IF NOT EXISTS InsertEfetuadoValido
+AFTER INSERT ON Efetuado
+WHEN New.idCompra IS NULL
 BEGIN
-insert into Compra(data, hora, total, nifCliente) VALUES (DATE('now'), TIME('now'), (SELECT new.quantidade*Produto.preco from Efetuado join Produto using (idProduto)), NULL);
-update Efetuado
-set idCompra = (SELECT last_insert_rowid())
-where idEfetuado = New.idEfetuado;
+INSERT INTO Compra(data, hora, total, nifCliente) VALUES (DATE('now'), TIME('now'), new.quantidade*(SELECT Produto.preco FROM Efetuado JOIN Produto USING (idProduto)), NULL);
+UPDATE Efetuado
+SET idCompra = (SELECT last_insert_rowid())
+WHERE idEfetuado = New.idEfetuado;
 END;
-
-
