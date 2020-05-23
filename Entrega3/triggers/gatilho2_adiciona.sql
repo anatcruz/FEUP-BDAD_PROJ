6fb352996705b867a3dfc533d73f5e6dc48e1c8c
andrenasx <andre2000.an@gmail.com>
2020-05-23 17:31:53 +0100

    more good nice

--- Entrega3/triggers/gatilho2_adiciona.sql
+++ Entrega3/triggers/gatilho2_adiciona.sql
@@ -1,4 +1,4 @@
--- Quando é criado um Efetuado é criado uma Compra com o idCompra seguinte, data e hora atuais e total calculado pela quantidade do produto selecionado, o nif é posto a NULL.
+-- Quando é criado um Efetuado é criada, caso não exista uma Compra com o id fornecido, uma nova Compra com o idCompra seguinte ao ultimo inserido, data e hora atuais e total calculado pela quantidade do produto selecionado, o nif é posto a NULL.
 
 PRAGMA foreign_keys = ON;
 
