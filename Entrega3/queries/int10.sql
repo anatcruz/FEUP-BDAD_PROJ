63fc3dc5b6e6e355af132cfddf604e0225ac605a
anatcruz <anateresacruz2000@gmail.com>
2020-05-23 20:23:12 +0100

    Small changes, tudo working

--- Entrega3/queries/int10.sql
+++ Entrega3/queries/int10.sql
@@ -4,9 +4,9 @@
 .headers on
 .nullvalue NULL
 
-SELECT nif, max(total) as Total, Loja.nome as Loja
+SELECT nif, max(total) AS Total, Loja.nome AS Loja
 FROM
 (SELECT nome, nif, idcompra, total
-FROM (Cliente NATURAL Join Pessoa) Join Compra
-where Cliente.nif = Compra.nifcliente) NATURAL join Efetuado join Produto using(idproduto) join Loja USING (idloja)
-GROUP by nif;
+FROM (Cliente NATURAL JOIN Pessoa) JOIN Compra
+WHERE (Cliente.nif = Compra.nifcliente)) NATURAL JOIN Efetuado JOIN Produto USING (idproduto) JOIN Loja USING (idloja)
+GROUP BY nif;
